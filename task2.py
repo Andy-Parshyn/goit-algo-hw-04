c5dd99c06590b40fa77c600139ef337dd72fd9eb
Andy-Parshyn <andriy.parshyn@gmail.com>
2025-12-01 11:47:24 +0100

    task2

---
+++ task2.py
@@ -0,0 +1,10 @@
+def merge_k_lists(lst):
+    res = []
+    for i in lst:
+        res.extend(i)
+    return res.sort()
+
+
+lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+merged_list = merge_k_lists(lists)
+print("Відсортований список:", merged_list)
